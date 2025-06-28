403b2eed6d7e500432bb24ddb656d490e1fa01d1
amrith-krishna <amrithkrishna@gmail.com>
2025-06-28 20:22:59 +0530

    Event Schema

--- Schema/Event.js
+++ Schema/Event.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const eventSchema = new mongoose.Schema({
-    name: String,
-    department: String,
-    venue: String,
-    startDate: Date,
-    endDate: Date,
-    description: String,
-    imagePath: String
-})
+  name: String,
+  department: String,
+  venue: String,
+  startDate: Date,
+  endDate: Date,
+  description: String,
+  imagePath: String,
+});
+
+export const Event = mongoose.model("Event", eventSchema);
